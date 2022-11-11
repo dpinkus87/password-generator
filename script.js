592550bae75710b513872c0e1c16344f5a85110f
dpinkus87 <dylanpinkus@gmail.com>
2022-11-10 18:41:05 -0600

    finalized generator

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var chosenChar = []
+var password = ""
 
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -34,35 +35,35 @@ function generatePassword() {
         }
 
         if (userSelectupperCase) {
-            chosenChar.push(upperCase)
+            chosenChar = chosenChar.concat(upperCase)
+            password = password+chosenChar[Math.floor(Math.random() * upperCase.length)]
+    
         }
         if (userSelectlowerrCase) {
-            chosenChar.push(lowerCase)
+            chosenChar = chosenChar.concat(lowerCase)
+            password = password+chosenChar[Math.floor(Math.random() * lowerCase.length)]
         }
         if (userSelectspecial) {
-            chosenChar.push(specialChar)
+            chosenChar = chosenChar.concat(specialChar)
+            password = password+chosenChar[Math.floor(Math.random() * specialChar.length)]
         }
         if (userSelectnumbers) {
-            chosenChar.push(number)
+            chosenChar = chosenChar.concat(number)
+            password = chosenChar[Math.floor(Math.random() * number.length)]
         }
     }
     characterSelection()
 
-    console.log(chosenChar)
+  
 
-    var password = ""
+    for (var index = 0; index < charLength; index++) {
+        console.log(chosenChar);
+        var randomChar = chosenChar[Math.floor(Math.random() * chosenChar.length)]
 
-    for (let index = 0; index < charLength; index++) {
-        // create var = 1 random character from the chosenChar list - (RANDOM INDEX Math)
-
-        var randomChar = Math.random(chosenChar)
-
-        // password += VAR-set ON 74
-
-        password += randomChar
+        var finalPassword = password += randomChar
     }
 
-    return password
+    return finalPassword
 }
 
 
